5ca95f9b34a6f52f43c775e66ba53cfa0ad35e8b
Daisuke Fujita <dtanshi45@gmail.com>
2015-03-27 17:37:20 +0900

    Implement DBSecurityGroup.tfstate

--- lib/terraforming/resource/db_security_group.rb
+++ lib/terraforming/resource/db_security_group.rb
@@ -5,8 +5,25 @@ module Terraforming::Resource
     end
 
     def self.tfstate(data)
-      # TODO: implement DBSecurityGroup.tfstate
-      raise NotImplementedError
+      tfstate_db_security_groups = data['DBSecurityGroups'].inject({}) do |result, security_group|
+        attributes = {
+          "db_subnet_group_name" => security_group['DBSecurityGroupName'],
+          "id" => security_group['DBSecurityGroupName'],
+          "ingress.#" => security_group['EC2SecurityGroups'].length.to_s,
+          "name" => security_group['DBSecurityGroupName'],
+        }
+
+        result["aws_db_security_group.#{security_group['DBSecurityGroupName']}"] = {
+          "type" => "aws_db_security_group",
+          "primary" => {
+            "id" => security_group['DBSecurityGroupName'],
+            "attributes" => attributes
+          }
+        }
+        result
+      end
+
+      JSON.pretty_generate(tfstate_db_security_groups)
     end
   end
 end
