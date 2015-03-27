a01b7586bfdadf4d5091064bd71d1d08f1bec12e
Daisuke Fujita <dtanshi45@gmail.com>
2015-03-27 16:08:11 +0900

    ERBize SecurityGroup

--- lib/terraforming/resource/security_group.rb
+++ lib/terraforming/resource/security_group.rb
@@ -1,39 +1,7 @@
 module Terraforming::Resource
   class SecurityGroup
     def self.tf(data)
-      data['SecurityGroups'].inject([]) do |result, security_group|
-        ingresses = security_group['IpPermissions'].map do |permission|
-      <<-EOS
-    ingress {
-        from_port   = #{permission['FromPort'] || 0}
-        to_port     = #{permission['ToPort'] || 0}
-        protocol    = "#{permission['IpProtocol']}"
-        cidr_blocks = #{permission['IpRanges'].map { |range| range['CidrIp'] }.inspect}
-    }
-      EOS
-        end.join("\n")
-
-        egresses = security_group['IpPermissionsEgress'].map do |permission|
-      <<-EOS
-    egress {
-        from_port   = #{permission['FromPort'] || 0}
-        to_port     = #{permission['ToPort'] || 0}
-        protocol    = "#{permission['IpProtocol']}"
-        cidr_blocks = #{permission['IpRanges'].map { |range| range['CidrIp'] }.inspect}
-    }
-      EOS
-        end.join("\n")
-
-        result << <<-EOS
-resource "aws_security_group" "#{security_group['GroupName']}" {
-    name        = "#{security_group['GroupName']}"
-    description = "#{security_group['Description']}"
-
-#{ingresses}
-#{egresses}
-}
-    EOS
-      end.join("\n")
+      ERB.new(open(Terraforming.template_path("tf/security_group")).read).result(binding)
     end
 
     def self.tfstate(data)
