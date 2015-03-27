442fb857ba5f023ee1dd65c31664c4a0ac332473
Daisuke Fujita <dtanshi45@gmail.com>
2015-03-27 16:14:10 +0900

    ERBize ELB

--- lib/terraforming/resource/elb.rb
+++ lib/terraforming/resource/elb.rb
@@ -1,39 +1,7 @@
 module Terraforming::Resource
   class ELB
     def self.tf(data)
-      data['LoadBalancerDescriptions'].inject([]) do |result, load_balancer|
-        listeners = load_balancer['ListenerDescriptions'].map { |ld| ld['Listener'] }.map do |listener|
-      <<-EOS
-    listener {
-        instance_port      = #{listener['InstancePort']}
-        instance_protocol  = "#{listener['InstanceProtocol'].downcase}"
-        lb_port            = #{listener['LoadBalancerPort']}
-        lb_protocol        = "#{listener['Protocol'].downcase}"
-        ssl_certificate_id = "#{listener['SSLCertificateId']}"
-    }
-      EOS
-        end.join("\n")
-
-        result << <<-EOS
-resource "aws_elb" "#{load_balancer['LoadBalancerName']}" {
-    name               = "#{load_balancer['LoadBalancerName']}"
-    availability_zones = #{load_balancer['AvailabilityZones'].inspect}
-    subnets            = #{load_balancer['Subnets'].inspect}
-    security_groups    = #{load_balancer['SecurityGroups'].inspect}
-    instances          = #{load_balancer['Instances'].map { |instance| instance['InstanceId'] }.inspect}
-
-#{listeners}
-
-    health_check {
-        healthy_threshold   = #{load_balancer['HealthCheck']['HealthyThreshold']}
-        unhealthy_threshold = #{load_balancer['HealthCheck']['UnhealthyThreshold']}
-        interval            = #{load_balancer['HealthCheck']['Interval']}
-        target              = "#{load_balancer['HealthCheck']['Target']}"
-        timeout             = #{load_balancer['HealthCheck']['Timeout']}
-    }
-}
-    EOS
-      end.join("\n")
+      ERB.new(open(Terraforming.template_path("tf/elb")).read).result(binding)
     end
 
     def self.tfstate(data)
