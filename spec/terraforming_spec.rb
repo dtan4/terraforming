7c72f6e59c8f7b4250157cce07027dea0a1f69d4
Daisuke Fujita <dtanshi45@gmail.com>
2015-03-29 15:22:22 +0900

    Remove needless test

--- spec/terraforming_spec.rb
+++ spec/terraforming_spec.rb
@@ -4,8 +4,4 @@ describe Terraforming do
   it 'has a version number' do
     expect(Terraforming::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
