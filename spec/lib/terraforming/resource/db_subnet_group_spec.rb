16fb2efe38909597505b04381dabb7b4b6244754
Daisuke Fujita <dtanshi45@gmail.com>
2015-03-30 11:36:51 +0900

    Add test for DBSubnetGroup.tfstate

--- spec/lib/terraforming/resource/db_subnet_group_spec.rb
+++ spec/lib/terraforming/resource/db_subnet_group_spec.rb
@@ -26,43 +26,10 @@ resource "aws_db_subnet_group" "fuga" {
     end
 
     describe ".tfstate" do
-      xit "should generate tfstate" do
-        expect(described_class.tfstate(json)).to eq JSON.pretty_generate({
-          "aws_elb.hoge" => {
-            "type" => "aws_elb",
-            "primary" => {
-              "id" => "hoge",
-              "attributes" => {
-                "availability_zones.#" => "2",
-                "dns_name" => "hoge-12345678.ap-northeast-1.elb.amazonaws.com",
-                "health_check.#" => "1",
-                "id" => "hoge",
-                "instances.#" => "1",
-                "listener.#" => "1",
-                "name" => "hoge",
-                "security_groups.#" => "2",
-                "subnets.#" => "2",
-              }
-            }
-          },
-          "aws_elb.fuga" => {
-            "type" => "aws_elb",
-            "primary" => {
-              "id" => "fuga",
-              "attributes" => {
-                "availability_zones.#" => "2",
-                "dns_name" => "fuga-90123456.ap-northeast-1.elb.amazonaws.com",
-                "health_check.#" => "1",
-                "id" => "fuga",
-                "instances.#" => "1",
-                "listener.#" => "1",
-                "name" => "fuga",
-                "security_groups.#" => "2",
-                "subnets.#" => "2",
-              }
-            }
-          }
-        })
+      it "should raise NotImplementedError" do
+        expect do
+          expect(described_class.tfstate(json))
+        end.to raise_error NotImplementedError
       end
     end
   end
