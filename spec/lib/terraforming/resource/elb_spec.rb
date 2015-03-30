36da3308e14f89abe978da71ac6b4b31b1357d9e
Daisuke Fujita <dtanshi45@gmail.com>
2015-03-30 10:30:44 +0900

    Add test for ELB.tfstate

--- spec/lib/terraforming/resource/elb_spec.rb
+++ spec/lib/terraforming/resource/elb_spec.rb
@@ -62,27 +62,39 @@ resource "aws_elb" "fuga" {
     end
 
     describe ".tfstate" do
-      xit "should generate tfstate" do
+      it "should generate tfstate" do
         expect(described_class.tfstate(json)).to eq JSON.pretty_generate({
-          "aws_s3_bucket.hoge" => {
-            "type" => "aws_s3_bucket",
+          "aws_elb.hoge" => {
+            "type" => "aws_elb",
             "primary" => {
               "id" => "hoge",
               "attributes" => {
-                "acl" => "private",
-                "bucket" => "hoge",
-                "id" => "hoge"
+                "availability_zones.#" => "2",
+                "dns_name" => "hoge-12345678.ap-northeast-1.elb.amazonaws.com",
+                "health_check.#" => "1",
+                "id" => "hoge",
+                "instances.#" => "1",
+                "listener.#" => "1",
+                "name" => "hoge",
+                "security_groups.#" => "2",
+                "subnets.#" => "2",
               }
             }
           },
-          "aws_s3_bucket.fuga" => {
-            "type" => "aws_s3_bucket",
+          "aws_elb.fuga" => {
+            "type" => "aws_elb",
             "primary" => {
               "id" => "fuga",
               "attributes" => {
-                "acl" => "private",
-                "bucket" => "fuga",
-                "id" => "fuga"
+                "availability_zones.#" => "2",
+                "dns_name" => "fuga-90123456.ap-northeast-1.elb.amazonaws.com",
+                "health_check.#" => "1",
+                "id" => "fuga",
+                "instances.#" => "1",
+                "listener.#" => "1",
+                "name" => "fuga",
+                "security_groups.#" => "2",
+                "subnets.#" => "2",
               }
             }
           }
