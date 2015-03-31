cfdd0f96840dfde199f4ca1b58d502faa49e802d
Daisuke Fujita <dtanshi45@gmail.com>
2015-03-31 20:44:10 +0900

    Modify RDS.tfstate to use aws-sdk client

--- lib/terraforming/resource/rds.rb
+++ lib/terraforming/resource/rds.rb
@@ -4,40 +4,40 @@ module Terraforming::Resource
       ERB.new(open(Terraforming.template_path("tf/rds")).read, nil, "-").result(binding)
     end
 
-    def self.tfstate(data, client = Aws::RDS::Client)
-      tfstate_db_instances = data['DBInstances'].inject({}) do |result, instance|
+    def self.tfstate(client = Aws::RDS::Client)
+      tfstate_db_instances = client.describe_db_instances.db_instances.inject({}) do |result, instance|
         attributes = {
-          "address" => instance['Endpoint']['Address'],
-          "allocated_storage" => instance['AllocatedStorage'].to_s,
-          "availability_zone" => instance['AvailabilityZone'],
-          "backup_retention_period" => instance['BackupRetentionPeriod'].to_s,
-          "backup_window" => instance['PreferredBackupWindow'],
-          "db_subnet_group_name" => instance['DBSubnetGroup'] ? instance['DBSubnetGroup']['DBSubnetGroupName'] : "",
-          "endpoint" => instance['Endpoint']['Address'],
-          "engine" => instance['Engine'],
-          "engine_version" => instance['EngineVersion'],
-          "final_snapshot_identifier" => "#{instance['DBInstanceIdentifier']}-final",
-          "id" => instance['DBInstanceIdentifier'],
-          "identifier" => instance['DBInstanceIdentifier'],
-          "instance_class" => instance['DBInstanceClass'],
-          "maintenance_window" => instance['PreferredMaintenanceWindow'],
-          "multi_az" => instance['MultiAZ'].to_s,
-          "name" => instance['DBName'],
-          "parameter_group_name" => instance['DBParameterGroups'][0]['DBParameterGroupName'],
+          "address" => instance.endpoint.address,
+          "allocated_storage" => instance.allocated_storage.to_s,
+          "availability_zone" => instance.availability_zone,
+          "backup_retention_period" => instance.backup_retention_period.to_s,
+          "backup_window" => instance.preferred_backup_window,
+          "db_subnet_group_name" => instance.db_subnet_group ? instance.db_subnet_group.db_subnet_group_name : "",
+          "endpoint" => instance.endpoint.address,
+          "engine" => instance.engine,
+          "engine_version" => instance.engine_version,
+          "final_snapshot_identifier" => "#{instance.db_instance_identifier}-final",
+          "id" => instance.db_instance_identifier,
+          "identifier" => instance.db_instance_identifier,
+          "instance_class" => instance.db_instance_class,
+          "maintenance_window" => instance.preferred_maintenance_window,
+          "multi_az" => instance.multi_az.to_s,
+          "name" => instance.db_name,
+          "parameter_group_name" => instance.db_parameter_groups[0].db_parameter_group_name,
           "password" => "xxxxxxxx",
-          "port" => instance['Endpoint']['Port'].to_s,
-          "publicly_accessible" => instance['PubliclyAccessible'].to_s,
-          "security_group_names.#" => instance['DBSecurityGroups'].length.to_s,
-          "status" => instance['DBInstanceStatus'],
-          "storage_type" => instance['StorageType'],
-          "username" => instance['MasterUsername'],
-          "vpc_security_group_ids.#" => instance['VpcSecurityGroups'].length.to_s,
+          "port" => instance.endpoint.port.to_s,
+          "publicly_accessible" => instance.publicly_accessible.to_s,
+          "security_group_names.#" => instance.db_security_groups.length.to_s,
+          "status" => instance.db_instance_status,
+          "storage_type" => instance.storage_type,
+          "username" => instance.master_username,
+          "vpc_security_group_ids.#" => instance.vpc_security_groups.length.to_s,
         }
 
-        result["aws_db_instance.#{instance['DBInstanceIdentifier']}"] = {
+        result["aws_db_instance.#{instance.db_instance_identifier}"] = {
           "type" => "aws_db_instance",
           "primary" => {
-            "id" => instance['DBInstanceIdentifier'],
+            "id" => instance.db_instance_identifier,
             "attributes" => attributes
           }
         }
