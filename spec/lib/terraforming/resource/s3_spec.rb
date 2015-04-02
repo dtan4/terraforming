469cab993ab6096e0faa52f92a984e86be276579
Daisuke Fujita <dtanshi45@gmail.com>
2015-04-02 13:24:41 +0900

    Fix S3 test to use aws-sdk

--- spec/lib/terraforming/resource/s3_spec.rb
+++ spec/lib/terraforming/resource/s3_spec.rb
@@ -2,13 +2,37 @@ require "spec_helper"
 
 module Terraforming::Resource
   describe S3 do
-    let(:json) do
-      JSON.parse(open(fixture_path("s3api/list-buckets")).read)
+    let(:buckets) do
+      [
+        {
+          creation_date: Time.parse("2014-01-01T12:12:12.000Z"),
+          name: "hoge"
+        },
+        {
+          creation_date: Time.parse("2015-01-01T00:00:00.000Z"),
+          name: "fuga"
+        }
+      ]
+    end
+
+    let(:client) do
+      Aws::S3::Client.new(stub_responses: true)
+    end
+
+    let(:owner)  do
+      {
+        display_name: "owner",
+        id: "12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh"
+      }
+    end
+
+    before do
+      client.stub_responses(:list_buckets, buckets: buckets, owner: owner)
     end
 
     describe ".tf" do
       it "should generate tf" do
-        expect(described_class.tf(json)).to eq <<-EOS
+        expect(described_class.tf(client)).to eq <<-EOS
 resource "aws_s3_bucket" "hoge" {
     bucket = "hoge"
     acl    = "private"
@@ -25,7 +49,7 @@ resource "aws_s3_bucket" "fuga" {
 
     describe ".tfstate" do
       it "should generate tfstate" do
-        expect(described_class.tfstate(json)).to eq JSON.pretty_generate({
+        expect(described_class.tfstate(client)).to eq JSON.pretty_generate({
           "aws_s3_bucket.hoge" => {
             "type" => "aws_s3_bucket",
             "primary" => {
