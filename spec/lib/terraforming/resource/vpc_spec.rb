67d545c74ecec4f0107384dfb87c770eb11027fa
Daisuke Fujita <dtanshi45@gmail.com>
2015-04-06 16:59:11 +0900

    Write test for VPC.tf

--- spec/lib/terraforming/resource/vpc_spec.rb
+++ spec/lib/terraforming/resource/vpc_spec.rb
@@ -2,6 +2,70 @@ require "spec_helper"
 
 module Terraforming::Resource
   describe VPC do
-    pending  "TODO: write tests..."
+    let(:client) do
+      Aws::EC2::Client.new(stub_responses: true)
+    end
+
+    let(:vpcs) do
+      [
+        {
+          vpc_id: "vpc-1234abcd",
+          state: "available",
+          cidr_block: "10.0.0.0/16",
+          dhcp_options_id: "dopt-1234abcd",
+          tags: [
+            {
+              key: "Name",
+              value: "hoge"
+            }
+          ],
+          instance_tenancy: "default",
+          is_default: false
+        },
+        {
+          vpc_id: "vpc-5678efgh",
+          state: "available",
+          cidr_block: "10.0.0.0/16",
+          dhcp_options_id: "dopt-5678efgh",
+          tags: [
+            {
+              key: "Name",
+              value: "fuga"
+            }
+          ],
+          instance_tenancy: "default",
+          is_default: false
+        }
+      ]
+    end
+
+    before do
+      client.stub_responses(:describe_vpcs, vpcs: vpcs)
+    end
+
+    describe ".tf" do
+      it "should generate tf" do
+        expect(described_class.tf(client)).to eq <<-EOS
+resource "aws_vpc" "hoge" {
+    cidr_block       = "10.0.0.0/16"
+    instance_tenancy = "default"
+
+    tags {
+        Name = "hoge"
+    }
+}
+
+resource "aws_vpc" "fuga" {
+    cidr_block       = "10.0.0.0/16"
+    instance_tenancy = "default"
+
+    tags {
+        Name = "fuga"
+    }
+}
+
+        EOS
+      end
+    end
   end
 end
