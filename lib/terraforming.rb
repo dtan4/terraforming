9dbeb084e0764dec33f7c3cd52445014110ea495
Daisuke Fujita <dtanshi45@gmail.com>
2015-04-06 16:24:38 +0900

    Move Terraform.template_path to Resource module

--- lib/terraforming.rb
+++ lib/terraforming.rb
@@ -16,9 +16,3 @@ require "terraforming/resource/rds"
 require "terraforming/resource/s3"
 require "terraforming/resource/security_group"
 require "terraforming/resource/vpc"
-
-module Terraforming
-  def self.template_path(template_name)
-    File.join(File.expand_path(File.dirname(__FILE__)), "terraforming", "template", template_name) << ".erb"
-  end
-end
--- lib/terraforming/resource.rb
+++ lib/terraforming/resource.rb
@@ -1,5 +1,9 @@
 module Terraforming::Resource
   def self.apply_template(client, erb)
-    ERB.new(open(Terraforming.template_path(erb)).read, nil, "-").result(binding)
+    ERB.new(open(template_path(erb)).read, nil, "-").result(binding)
+  end
+
+  def self.template_path(template_name)
+    File.join(File.expand_path(File.dirname(__FILE__)), "template", template_name) << ".erb"
   end
 end
