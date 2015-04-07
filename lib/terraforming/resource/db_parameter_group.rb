e3f7435c01932a8c900696a4a94ce01049cda48f
Daisuke Fujita <dtanshi45@gmail.com>
2015-04-07 17:48:35 +0900

    Implement DBParameterGroup.tfstate

--- lib/terraforming/resource/db_parameter_group.rb
+++ lib/terraforming/resource/db_parameter_group.rb
@@ -5,9 +5,29 @@ module Terraforming::Resource
       Terraforming::Resource.apply_template(client, "tf/db_parameter_group")
     end
 
-    def self.tfstate(data)
+    def self.tfstate(client = Aws::RDS::Client.new)
       # TODO: implement DBParameterGroup.tfstate
-      raise NotImplementedError
+      tfstate_db_parameter_groups =
+        client.describe_db_parameter_groups.db_parameter_groups.inject({}) do |result, parameter_group|
+        attributes = {
+          "description" => parameter_group.description,
+          "family" => parameter_group.db_parameter_group_family,
+          "id" => parameter_group.db_parameter_group_name,
+          "name" => parameter_group.db_parameter_group_name,
+          "parameter.#" => client.describe_db_parameters(db_parameter_group_name: parameter_group.db_parameter_group_name).parameters.length.to_s
+        }
+        result["aws_db_parameter_group.#{parameter_group.db_parameter_group_name}"] = {
+          "type" => "aws_db_parameter_group",
+          "primary" => {
+            "id" => parameter_group.db_parameter_group_name,
+            "attributes" => attributes
+          }
+        }
+
+        result
+      end
+
+      JSON.pretty_generate(tfstate_db_parameter_groups)
     end
   end
 end
