5480fcb65cc6e4da8ad74decb69ebd4641a0ee7e
Daisuke Fujita <dtanshi45@gmail.com>
2015-04-09 09:52:05 +0900

    Generate entire tfstate of S3

--- lib/terraforming/resource/s3.rb
+++ lib/terraforming/resource/s3.rb
@@ -5,7 +5,7 @@ module Terraforming::Resource
     end
 
     def self.tfstate(client = Aws::S3::Client.new)
-      tfstate_s3_buckets = client.list_buckets.buckets.inject({}) do |result, bucket|
+      resources = client.list_buckets.buckets.inject({}) do |result, bucket|
         result["aws_s3_bucket.#{bucket.name}"] = {
           "type" => "aws_s3_bucket",
           "primary" => {
@@ -21,7 +21,19 @@ module Terraforming::Resource
         result
       end
 
-      JSON.pretty_generate(tfstate_s3_buckets)
+      tfstate = {
+        "version" => 1,
+        "serial" => 84,
+        "modules" => {
+          "path" => [
+            "root"
+          ],
+          "outputs" => {},
+          "resources" => resources
+        }
+      }
+
+      JSON.pretty_generate(tfstate)
     end
   end
 end
