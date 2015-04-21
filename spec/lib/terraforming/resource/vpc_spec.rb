3a1225e5ac2a42348d4574060c7744fbd9cbb16a
Daisuke Fujita <dtanshi45@gmail.com>
2015-04-21 18:12:06 +0900

    Generate VPC tf with vpc attributes

--- spec/lib/terraforming/resource/vpc_spec.rb
+++ spec/lib/terraforming/resource/vpc_spec.rb
@@ -57,8 +57,10 @@ module Terraforming::Resource
       it "should generate tf" do
         expect(described_class.tf(client)).to eq <<-EOS
 resource "aws_vpc" "hoge" {
-    cidr_block       = "10.0.0.0/16"
-    instance_tenancy = "default"
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+    instance_tenancy     = "default"
 
     tags {
         Name = "hoge"
@@ -66,8 +68,10 @@ resource "aws_vpc" "hoge" {
 }
 
 resource "aws_vpc" "fuga" {
-    cidr_block       = "10.0.0.0/16"
-    instance_tenancy = "default"
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+    instance_tenancy     = "default"
 
     tags {
         Name = "fuga"
