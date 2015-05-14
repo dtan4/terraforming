63f0f7ed4450bb27a6cdcdc92ff61557d37861ed
Daisuke Fujita <dtanshi45@gmail.com>
2015-05-14 10:49:13 +0900

    Generate IAM group policy tf

---
+++ lib/terraforming/resource/iam_group_policy.rb
@@ -0,0 +1,68 @@
+module Terraforming
+  module Resource
+    class IAMGroupPolicy
+      include Terraforming::Util
+
+      def self.tf(client = Aws::IAM::Client.new)
+        self.new(client).tf
+      end
+
+      def self.tfstate(client = Aws::IAM::Client.new)
+        self.new(client).tfstate
+      end
+
+      def initialize(client)
+        @client = client
+      end
+
+      def tf
+        apply_template(@client, "tf/iam_group_policy")
+      end
+
+      def tfstate
+        resources = iam_user_policies.inject({}) do |result, policy|
+          attributes = {
+            "id" => iam_user_policy_id_of(policy),
+            "name" => policy.policy_name,
+            "policy" => CGI.unescape(policy.policy_document)
+          }
+          result["aws_iam_user_policy.#{policy.policy_name}"] = {
+            "type" => "aws_iam_user_policy",
+            "primary" => {
+              "id" => iam_user_policy_id_of(policy),
+              "attributes" => attributes
+            }
+          }
+
+          result
+        end
+
+        generate_tfstate(resources)
+      end
+
+      private
+
+      def iam_groups
+        @client.list_groups.groups
+      end
+
+      def iam_group_policy_id_of(policy)
+        "#{policy.group_name}:#{policy.policy_name}"
+      end
+
+      def iam_group_policy_names_in(group)
+        @client.list_group_policies(group_name: group.group_name).policy_names
+      end
+
+      def iam_group_policy_of(group, policy_name)
+        @client.get_group_policy(group_name: group.group_name, policy_name: policy_name)
+      end
+
+      def iam_group_policies
+        iam_groups.map do |group|
+          iam_group_policy_names_in(group).map { |policy_name| iam_group_policy_of(group, policy_name) }
+        end.flatten
+      end
+    end
+  end
+end
---
+++ spec/lib/terraforming/resource/iam_group_policy_spec.rb
@@ -0,0 +1,136 @@
+require "spec_helper"
+
+module Terraforming
+  module Resource
+    describe IAMGroupPolicy do
+      let(:client) do
+        Aws::IAM::Client.new(stub_responses: true)
+      end
+
+      let(:groups) do
+        [
+          {
+            path: "/",
+            group_name: "hoge",
+            group_id: "ABCDEFGHIJKLMN1234567",
+            arn: "arn:aws:iam::123456789012:group/hoge",
+            create_date: Time.parse("2015-04-01 12:34:56 UTC"),
+          },
+          {
+            path: "/system/",
+            group_name: "fuga",
+            group_id: "OPQRSTUVWXYZA8901234",
+            arn: "arn:aws:iam::345678901234:group/fuga",
+            create_date: Time.parse("2015-05-01 12:34:56 UTC"),
+          },
+        ]
+      end
+
+      let(:hoge_policy) do
+        {
+          group_name: "hoge",
+          policy_name: "hoge_policy",
+          policy_document: "%7B%0A%20%20%22Version%22%3A%20%222012-10-17%22%2C%0A%20%20%22Statement%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22Action%22%3A%20%5B%0A%20%20%20%20%20%20%20%20%22ec2%3ADescribe%2A%22%0A%20%20%20%20%20%20%5D%2C%0A%20%20%20%20%20%20%22Effect%22%3A%20%22Allow%22%2C%0A%20%20%20%20%20%20%22Resource%22%3A%20%22%2A%22%0A%20%20%20%20%7D%0A%20%20%5D%0A%7D%0A",
+        }
+      end
+
+      let(:fuga_policy) do
+        {
+          group_name: "fuga",
+          policy_name: "fuga_policy",
+          policy_document: "%7B%0A%20%20%22Version%22%3A%20%222012-10-17%22%2C%0A%20%20%22Statement%22%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22Action%22%3A%20%5B%0A%20%20%20%20%20%20%20%20%22ec2%3ADescribe%2A%22%0A%20%20%20%20%20%20%5D%2C%0A%20%20%20%20%20%20%22Effect%22%3A%20%22Allow%22%2C%0A%20%20%20%20%20%20%22Resource%22%3A%20%22%2A%22%0A%20%20%20%20%7D%0A%20%20%5D%0A%7D%0A",
+        }
+      end
+
+      before do
+        client.stub_responses(:list_groups, groups: groups)
+        client.stub_responses(:list_group_policies, [{ policy_names: %w(hoge_policy)}, { policy_names: %w(fuga_policy) }])
+        client.stub_responses(:get_group_policy, [hoge_policy, fuga_policy])
+      end
+
+      describe ".tf" do
+        it "should generate tf" do
+          expect(described_class.tf(client)).to eq <<-EOS
+resource "aws_iam_group_policy" "hoge_policy" {
+    name   = "hoge_policy"
+    group  = "hoge"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_group_policy" "fuga_policy" {
+    name   = "fuga_policy"
+    group  = "fuga"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+        EOS
+        end
+      end
+
+      describe ".tfstate" do
+        xit "should generate tfstate" do
+          expect(described_class.tfstate(client)).to eq JSON.pretty_generate({
+            "version" => 1,
+            "serial" => 1,
+            "modules" => {
+              "path" => [
+                "root"
+              ],
+              "outputs" => {},
+              "resources" => {
+                "aws_iam_user_policy.hoge_policy" => {
+                  "type" => "aws_iam_user_policy",
+                  "primary" => {
+                    "id" => "hoge:hoge_policy",
+                    "attributes" => {
+                      "id" => "hoge:hoge_policy",
+                      "name" => "hoge_policy",
+                      "policy" => "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": [\n        \"ec2:Describe*\"\n      ],\n      \"Effect\": \"Allow\",\n      \"Resource\": \"*\"\n    }\n  ]\n}\n",
+                    }
+                  }
+                },
+                "aws_iam_user_policy.fuga_policy" => {
+                  "type" => "aws_iam_user_policy",
+                  "primary" => {
+                    "id" => "fuga:fuga_policy",
+                    "attributes" => {
+                      "id" => "fuga:fuga_policy",
+                      "name" => "fuga_policy",
+                      "policy" => "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": [\n        \"ec2:Describe*\"\n      ],\n      \"Effect\": \"Allow\",\n      \"Resource\": \"*\"\n    }\n  ]\n}\n",
+                    }
+                  }
+                },
+              }
+            }
+          })
+        end
+      end
+    end
+  end
+end
