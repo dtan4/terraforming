b4c0e2cb1cac7f01b3100b433e7602699c4882a0
Daisuke Fujita <dtanshi45@gmail.com>
2015-06-20 00:10:42 +0900

    Add merge tfstate feature to S3

--- spec/lib/terraforming/resource/s3_spec.rb
+++ spec/lib/terraforming/resource/s3_spec.rb
@@ -49,43 +49,110 @@ resource "aws_s3_bucket" "fuga" {
       end
 
       describe ".tfstate" do
-        it "should generate tfstate" do
-          expect(described_class.tfstate(client: client)).to eq JSON.pretty_generate({
-            "version" => 1,
-            "serial" => 1,
-            "modules" => [
-              {
-                "path" => [
-                  "root"
-                ],
-                "outputs" => {},
-                "resources" => {
-                  "aws_s3_bucket.hoge" => {
-                    "type" => "aws_s3_bucket",
-                    "primary" => {
-                      "id" => "hoge",
-                      "attributes" => {
-                        "acl" => "private",
-                        "bucket" => "hoge",
-                        "id" => "hoge"
+        context "without existing tfstate" do
+          it "should generate tfstate" do
+            expect(described_class.tfstate(client: client)).to eq JSON.pretty_generate({
+              "version" => 1,
+              "serial" => 1,
+              "modules" => [
+                {
+                  "path" => [
+                    "root"
+                  ],
+                  "outputs" => {},
+                  "resources" => {
+                    "aws_s3_bucket.hoge" => {
+                      "type" => "aws_s3_bucket",
+                      "primary" => {
+                        "id" => "hoge",
+                        "attributes" => {
+                          "acl" => "private",
+                          "bucket" => "hoge",
+                          "id" => "hoge"
+                        }
                       }
-                    }
-                  },
-                  "aws_s3_bucket.fuga" => {
-                    "type" => "aws_s3_bucket",
-                    "primary" => {
-                      "id" => "fuga",
-                      "attributes" => {
-                        "acl" => "private",
-                        "bucket" => "fuga",
-                        "id" => "fuga"
+                    },
+                    "aws_s3_bucket.fuga" => {
+                      "type" => "aws_s3_bucket",
+                      "primary" => {
+                        "id" => "fuga",
+                        "attributes" => {
+                          "acl" => "private",
+                          "bucket" => "fuga",
+                          "id" => "fuga"
+                        }
                       }
                     }
                   }
                 }
-              }
-            ]
-          })
+              ]
+            })
+          end
+        end
+
+        context "with existing tfstate" do
+          it "should generate tfstate and merge it to existing tfstate" do
+            expect(described_class.tfstate(client: client, tfstate_base: tfstate_fixture)).to eq JSON.pretty_generate({
+              "version" => 1,
+              "serial" => 88,
+              "remote" => {
+                "type" => "s3",
+                "config" => { "bucket" => "terraforming-tfstate", "key" => "tf" }
+              },
+              "modules" => [
+                {
+                  "path" => ["root"],
+                  "outputs" => {},
+                  "resources" => {
+                    "aws_elb.hogehoge" => {
+                      "type" => "aws_elb",
+                      "primary" => {
+                        "id" => "hogehoge",
+                        "attributes" => {
+                          "availability_zones.#" => "2",
+                          "connection_draining" => "true",
+                          "connection_draining_timeout" => "300",
+                          "cross_zone_load_balancing" => "true",
+                          "dns_name" => "hoge-12345678.ap-northeast-1.elb.amazonaws.com",
+                          "health_check.#" => "1",
+                          "id" => "hogehoge",
+                          "idle_timeout" => "60",
+                          "instances.#" => "1",
+                          "listener.#" => "1",
+                          "name" => "hoge",
+                          "security_groups.#" => "2",
+                          "source_security_group" => "default",
+                          "subnets.#" => "2"
+                        }
+                      }
+                    },
+                    "aws_s3_bucket.hoge" => {
+                      "type" => "aws_s3_bucket",
+                      "primary" => {
+                        "id" => "hoge",
+                        "attributes" => {
+                          "acl" => "private",
+                          "bucket" => "hoge",
+                          "id" => "hoge"
+                        }
+                      }
+                    },
+                    "aws_s3_bucket.fuga" => {
+                      "type" => "aws_s3_bucket",
+                      "primary" => {
+                        "id" => "fuga",
+                        "attributes" => {
+                          "acl" => "private",
+                          "bucket" => "fuga",
+                          "id" => "fuga"
+                        }
+                      }
+                    },
+                  }
+                }
+              ]
+            })
+          end
         end
       end
     end
