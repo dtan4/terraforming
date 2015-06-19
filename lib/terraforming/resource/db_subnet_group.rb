e2dd075fd1e0372abe62e148c1ab16278f21f67f
Daisuke Fujita <dtanshi45@gmail.com>
2015-06-19 23:56:24 +0900

    Add merge tfstate feature to DBSubnetGroup

--- lib/terraforming/resource/db_subnet_group.rb
+++ lib/terraforming/resource/db_subnet_group.rb
@@ -7,8 +7,8 @@ module Terraforming
         self.new(client).tf
       end
 
-      def self.tfstate(client: Aws::RDS::Client.new)
-        self.new(client).tfstate
+      def self.tfstate(client: Aws::RDS::Client.new, tfstate_base: nil)
+        self.new(client).tfstate(tfstate_base)
       end
 
       def initialize(client)
@@ -19,7 +19,7 @@ module Terraforming
         apply_template(@client, "tf/db_subnet_group")
       end
 
-      def tfstate
+      def tfstate(tfstate_base)
         resources = db_subnet_groups.inject({}) do |result, subnet_group|
           attributes = {
             "description" => subnet_group.db_subnet_group_description,
@@ -37,7 +37,7 @@ module Terraforming
           result
         end
 
-        generate_tfstate(resources)
+        generate_tfstate(resources, tfstate_base)
       end
 
       private
--- spec/lib/terraforming/resource/db_subnet_group_spec.rb
+++ spec/lib/terraforming/resource/db_subnet_group_spec.rb
@@ -80,43 +80,110 @@ resource "aws_db_subnet_group" "fuga" {
       end
 
       describe ".tfstate" do
-        it "should generate tfstate" do
-          expect(described_class.tfstate(client: client)).to eq JSON.pretty_generate({
-            "version" => 1,
-            "serial" => 1,
-            "modules" => [
-              {
-                "path" => [
-                  "root"
-                ],
-                "outputs" => {},
-                "resources" => {
-                  "aws_db_subnet_group.hoge" => {
-                    "type" => "aws_db_subnet_group",
-                    "primary" => {
-                      "id" => "hoge",
-                      "attributes" => {
-                        "description" => "DB subnet group hoge",
-                        "name" => "hoge",
-                        "subnet_ids.#" => "2",
+        context "without existing tfstate" do
+          it "should generate tfstate" do
+            expect(described_class.tfstate(client: client)).to eq JSON.pretty_generate({
+              "version" => 1,
+              "serial" => 1,
+              "modules" => [
+                {
+                  "path" => [
+                    "root"
+                  ],
+                  "outputs" => {},
+                  "resources" => {
+                    "aws_db_subnet_group.hoge" => {
+                      "type" => "aws_db_subnet_group",
+                      "primary" => {
+                        "id" => "hoge",
+                        "attributes" => {
+                          "description" => "DB subnet group hoge",
+                          "name" => "hoge",
+                          "subnet_ids.#" => "2",
+                        }
                       }
-                    }
-                  },
-                  "aws_db_subnet_group.fuga" => {
-                    "type" => "aws_db_subnet_group",
-                    "primary" => {
-                      "id" => "fuga",
-                      "attributes" => {
-                        "description" => "DB subnet group fuga",
-                        "name" => "fuga",
-                        "subnet_ids.#" => "2",
+                    },
+                    "aws_db_subnet_group.fuga" => {
+                      "type" => "aws_db_subnet_group",
+                      "primary" => {
+                        "id" => "fuga",
+                        "attributes" => {
+                          "description" => "DB subnet group fuga",
+                          "name" => "fuga",
+                          "subnet_ids.#" => "2",
+                        }
                       }
                     }
                   }
                 }
-              }
-            ]
-          })
+              ]
+            })
+          end
+        end
+
+        context "with existing tfstate" do
+          it "should generate tfstate and merge it to existing tfstate" do
+            expect(described_class.tfstate(client: client, tfstate_base: tfstate_fixture)).to eq JSON.pretty_generate({
+              "version" => 1,
+              "serial" => 88,
+              "remote" => {
+                "type" => "s3",
+                "config" => { "bucket" => "terraforming-tfstate", "key" => "tf" }
+              },
+              "modules" => [
+                {
+                  "path" => ["root"],
+                  "outputs" => {},
+                  "resources" => {
+                    "aws_elb.hogehoge" => {
+                      "type" => "aws_elb",
+                      "primary" => {
+                        "id" => "hogehoge",
+                        "attributes" => {
+                          "availability_zones.#" => "2",
+                          "connection_draining" => "true",
+                          "connection_draining_timeout" => "300",
+                          "cross_zone_load_balancing" => "true",
+                          "dns_name" => "hoge-12345678.ap-northeast-1.elb.amazonaws.com",
+                          "health_check.#" => "1",
+                          "id" => "hogehoge",
+                          "idle_timeout" => "60",
+                          "instances.#" => "1",
+                          "listener.#" => "1",
+                          "name" => "hoge",
+                          "security_groups.#" => "2",
+                          "source_security_group" => "default",
+                          "subnets.#" => "2"
+                        }
+                      }
+                    },
+                    "aws_db_subnet_group.hoge" => {
+                      "type" => "aws_db_subnet_group",
+                      "primary" => {
+                        "id" => "hoge",
+                        "attributes" => {
+                          "description" => "DB subnet group hoge",
+                          "name" => "hoge",
+                          "subnet_ids.#" => "2",
+                        }
+                      }
+                    },
+                    "aws_db_subnet_group.fuga" => {
+                      "type" => "aws_db_subnet_group",
+                      "primary" => {
+                        "id" => "fuga",
+                        "attributes" => {
+                          "description" => "DB subnet group fuga",
+                          "name" => "fuga",
+                          "subnet_ids.#" => "2",
+                        }
+                      }
+                    }
+                  }
+                }
+              ]
+            })
+          end
         end
       end
     end
