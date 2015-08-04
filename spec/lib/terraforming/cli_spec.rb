2f4899f896e8d80002eec71b295aeed3ebf74e8f
Daisuke Fujita <dtanshi45@gmail.com>
2015-08-05 01:05:47 +0900

    Move existing specs to new context

--- spec/lib/terraforming/cli_spec.rb
+++ spec/lib/terraforming/cli_spec.rb
@@ -3,10 +3,6 @@ require "spec_helper"
 module Terraforming
   describe CLI do
     shared_examples "CLI examples" do
-      before do
-        allow(STDOUT).to receive(:puts).and_return(nil)
-      end
-
       context "without --tfstate" do
         it "should export DBParameterGroup tf" do
           expect(klass).to receive(:tf)
@@ -29,172 +25,178 @@ module Terraforming
       end
     end
 
-    describe "dbpg" do
-      let(:klass)   { Terraforming::Resource::DBParameterGroup }
-      let(:command) { :dbpg }
+    context "resources" do
+      before do
+        allow(STDOUT).to receive(:puts).and_return(nil)
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "dbpg" do
+        let(:klass)   { Terraforming::Resource::DBParameterGroup }
+        let(:command) { :dbpg }
 
-    describe "dbsg" do
-      let(:klass)   { Terraforming::Resource::DBSecurityGroup }
-      let(:command) { :dbsg }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "dbsg" do
+        let(:klass)   { Terraforming::Resource::DBSecurityGroup }
+        let(:command) { :dbsg }
 
-    describe "dbsn" do
-      let(:klass)   { Terraforming::Resource::DBSubnetGroup }
-      let(:command) { :dbsn }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "dbsn" do
+        let(:klass)   { Terraforming::Resource::DBSubnetGroup }
+        let(:command) { :dbsn }
 
-    describe "ec2" do
-      let(:klass)   { Terraforming::Resource::EC2 }
-      let(:command) { :ec2 }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "ec2" do
+        let(:klass)   { Terraforming::Resource::EC2 }
+        let(:command) { :ec2 }
 
-    describe "ecc" do
-      let(:klass)   { Terraforming::Resource::ElastiCacheCluster }
-      let(:command) { :ecc }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "ecc" do
+        let(:klass)   { Terraforming::Resource::ElastiCacheCluster }
+        let(:command) { :ecc }
 
-    describe "ecsn" do
-      let(:klass)   { Terraforming::Resource::ElastiCacheSubnetGroup }
-      let(:command) { :ecsn }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "ecsn" do
+        let(:klass)   { Terraforming::Resource::ElastiCacheSubnetGroup }
+        let(:command) { :ecsn }
 
-    describe "elb" do
-      let(:klass)   { Terraforming::Resource::ELB }
-      let(:command) { :elb }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "elb" do
+        let(:klass)   { Terraforming::Resource::ELB }
+        let(:command) { :elb }
 
-    describe "iamg" do
-      let(:klass)   { Terraforming::Resource::IAMGroup }
-      let(:command) { :iamg }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamg" do
+        let(:klass)   { Terraforming::Resource::IAMGroup }
+        let(:command) { :iamg }
 
-    describe "iamgm" do
-      let(:klass)   { Terraforming::Resource::IAMGroupMembership }
-      let(:command) { :iamgm }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamgm" do
+        let(:klass)   { Terraforming::Resource::IAMGroupMembership }
+        let(:command) { :iamgm }
 
-    describe "iamgp" do
-      let(:klass)   { Terraforming::Resource::IAMGroupPolicy }
-      let(:command) { :iamgp }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamgp" do
+        let(:klass)   { Terraforming::Resource::IAMGroupPolicy }
+        let(:command) { :iamgp }
 
-    describe "iamip" do
-      let(:klass)   { Terraforming::Resource::IAMInstanceProfile }
-      let(:command) { :iamip }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamip" do
+        let(:klass)   { Terraforming::Resource::IAMInstanceProfile }
+        let(:command) { :iamip }
 
-    describe "iamp" do
-      let(:klass)   { Terraforming::Resource::IAMPolicy }
-      let(:command) { :iamp }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamp" do
+        let(:klass)   { Terraforming::Resource::IAMPolicy }
+        let(:command) { :iamp }
 
-    describe "iamr" do
-      let(:klass)   { Terraforming::Resource::IAMRole }
-      let(:command) { :iamr }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamr" do
+        let(:klass)   { Terraforming::Resource::IAMRole }
+        let(:command) { :iamr }
 
-    describe "iamrp" do
-      let(:klass)   { Terraforming::Resource::IAMRolePolicy }
-      let(:command) { :iamrp }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamrp" do
+        let(:klass)   { Terraforming::Resource::IAMRolePolicy }
+        let(:command) { :iamrp }
 
-    describe "iamu" do
-      let(:klass)   { Terraforming::Resource::IAMUser }
-      let(:command) { :iamu }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamu" do
+        let(:klass)   { Terraforming::Resource::IAMUser }
+        let(:command) { :iamu }
 
-    describe "iamup" do
-      let(:klass)   { Terraforming::Resource::IAMUserPolicy }
-      let(:command) { :iamup }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "iamup" do
+        let(:klass)   { Terraforming::Resource::IAMUserPolicy }
+        let(:command) { :iamup }
 
-    describe "nacl" do
-      let(:klass)   { Terraforming::Resource::NetworkACL }
-      let(:command) { :nacl }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "nacl" do
+        let(:klass)   { Terraforming::Resource::NetworkACL }
+        let(:command) { :nacl }
 
-    describe "r53r" do
-      let(:klass)   { Terraforming::Resource::Route53Record }
-      let(:command) { :r53r }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "r53r" do
+        let(:klass)   { Terraforming::Resource::Route53Record }
+        let(:command) { :r53r }
 
-    describe "r53z" do
-      let(:klass)   { Terraforming::Resource::Route53Zone }
-      let(:command) { :r53z }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "r53z" do
+        let(:klass)   { Terraforming::Resource::Route53Zone }
+        let(:command) { :r53z }
 
-    describe "RDS" do
-      let(:klass)   { Terraforming::Resource::RDS }
-      let(:command) { :rds }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "RDS" do
+        let(:klass)   { Terraforming::Resource::RDS }
+        let(:command) { :rds }
 
-    describe "s3" do
-      let(:klass)   { Terraforming::Resource::S3 }
-      let(:command) { :s3 }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "s3" do
+        let(:klass)   { Terraforming::Resource::S3 }
+        let(:command) { :s3 }
 
-    describe "sg" do
-      let(:klass)   { Terraforming::Resource::SecurityGroup }
-      let(:command) { :sg }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "sg" do
+        let(:klass)   { Terraforming::Resource::SecurityGroup }
+        let(:command) { :sg }
 
-    describe "sn" do
-      let(:klass)   { Terraforming::Resource::Subnet }
-      let(:command) { :sn }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
-    end
+      describe "sn" do
+        let(:klass)   { Terraforming::Resource::Subnet }
+        let(:command) { :sn }
 
-    describe "vpc" do
-      let(:klass)   { Terraforming::Resource::VPC }
-      let(:command) { :vpc }
+        it_behaves_like "CLI examples"
+      end
 
-      it_behaves_like "CLI examples"
+      describe "vpc" do
+        let(:klass)   { Terraforming::Resource::VPC }
+        let(:command) { :vpc }
+
+        it_behaves_like "CLI examples"
+      end
     end
   end
 end
