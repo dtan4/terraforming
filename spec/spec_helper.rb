13e4528e2248b085db2cbae5f413d34ae81085e5
Troy Jendra <troy@troyjendra.com>
2015-12-01 12:45:03 +1100

    Use the .new method on MultiFormatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 require "codeclimate-test-reporter"
 
 SimpleCov.start do
-  formatter SimpleCov::Formatter::MultiFormatter[
+  formatter SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
     CodeClimate::TestReporter::Formatter,
-  ]
+  ])
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
