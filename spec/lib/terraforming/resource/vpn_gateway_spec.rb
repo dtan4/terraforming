d4769c472d135ef8ec959052068b261ddd16e177
tmccabe07 <tmccabe07@users.noreply.github.com>
2016-04-17 13:33:55 -0400

    Update vpn_gateway_spec.rb
    fixed : to = sign in expected result

--- spec/lib/terraforming/resource/vpn_gateway_spec.rb
+++ spec/lib/terraforming/resource/vpn_gateway_spec.rb
@@ -44,14 +44,14 @@ module Terraforming
           expect(described_class.tf(client: client)).to eq <<-EOS
 resource "aws_vpn_gateway" "vgw-1234abcd" {
     vpc_id = "vpc-1234abcd"
-    availability_zone: "us-east-1c"
+    availability_zone = "us-east-1c"
     tags {
     }
 }
 
 resource "aws_vpn_gateway" "test" {
     vpc_id = "vpc-5678efgh"
-    availability_zone: "us-east-1d"
+    availability_zone = "us-east-1d"
     tags {
         "Name" = "test"
     }
