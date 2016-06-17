3f21f19aec74213fcdddaa42034f3b20cc3e5c21
DJRH <Daniel.Home@sainsburys.co.uk>
2016-06-17 17:28:38 +0100

    Add us-east-1 as region
    OpsWorks is only available in us-east-1

--- lib/terraforming/resource/opsworks_custom_layer.rb
+++ lib/terraforming/resource/opsworks_custom_layer.rb
@@ -3,11 +3,11 @@ module Terraforming
     class OpsWorksCustomLayer
       include Terraforming::Util
 
-      def self.tf(client: Aws::OpsWorks::Client.new)
+      def self.tf(client: Aws::OpsWorks::Client.new(region: 'us-east-1'))
         self.new(client).tf
       end
 
-      def self.tfstate(client: Aws::OpsWorks::Client.new)
+      def self.tfstate(client: Aws::OpsWorks::Client.new(region: 'us-east-1'))
         self.new(client).tfstate
       end
 
