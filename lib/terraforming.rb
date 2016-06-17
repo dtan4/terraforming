37ae66c7d073a330fe33874e02f8661acecd1afa
Genki Sugawara <sugawara@cookpad.com>
2016-06-17 14:15:20 +0900

    Fix ox load error

--- lib/terraforming.rb
+++ lib/terraforming.rb
@@ -1,5 +1,12 @@
 require "oj"
-require "ox"
+
+begin
+  require "ox"
+rescue NameError => e
+  spec = Gem::Specification.stubs.find {|s| s.name == 'ox' }
+  raise e unless spec
+  require File.join(spec.gem_dir, "lib/ox")
+end
 
 require "aws-sdk-core"
 require "erb"
