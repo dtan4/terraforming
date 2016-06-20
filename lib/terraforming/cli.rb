a7e949471099995d313dbdc7ccd944d6290038f4
DJ Home <Daniel.Home@sainsburys.co.uk>
2016-06-20 11:28:41 +0100

    Alphabetical ordering

--- lib/terraforming/cli.rb
+++ lib/terraforming/cli.rb
@@ -121,14 +121,14 @@ module Terraforming
       execute(Terraforming::Resource::NetworkInterface, options)
     end
 
-    desc "opws", "OpsWorks Stack"
-    def opws
-      execute(Terraforming::Resource::OpsWorksStack, options)
-    end
-
     desc "opwcl", "OpsWorks Custom Layer"
     def opwcl
       execute(Terraforming::Resource::OpsWorksCustomLayer, options)
+    end
+
+    desc "opws", "OpsWorks Stack"
+    def opws
+      execute(Terraforming::Resource::OpsWorksStack, options)
     end
 
     desc "r53r", "Route53 Record"
