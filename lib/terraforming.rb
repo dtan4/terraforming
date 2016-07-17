4495a588585b3c11e0c45006e2dd9ea5cc108d8e
Matt Gartman <mattgartman@gmail.com>
2016-07-17 00:08:58 -0400

    finished lambda tfstate, bug fixes and refactoring

--- lib/terraforming.rb
+++ lib/terraforming.rb
@@ -14,6 +14,7 @@ require "erb"
 require "json"
 require "thor"
 require "zlib"
+require "net/http"
 
 require "terraforming/util"
 require "terraforming/version"
--- lib/terraforming/resource/lambda_function.rb
+++ lib/terraforming/resource/lambda_function.rb
@@ -20,16 +20,47 @@ module Terraforming
       end
 
       def tfstate
-        db_instances.inject({}) do |resources, instance|
+        lambda_functions.inject({}) do |resources, instance|
           attributes = {
-              "filename" => instance.endpoint.address,
+              "arn" => instance.configuration.function_arn,
+              "description" => instance.configuration.description,
+              "filename" => "#{instance.configuration.function_name}.zip",
+              "function_name" => instance.configuration.function_name,
+              "handler" => instance.configuration.handler,
+              "id" => instance.configuration.function_name,
+              "last_modified" => instance.configuration
+                                         .last_modified
+                                         .strftime("%FT%T%z"),
+              "memory_size" => instance.configuration.memory_size.to_s,
+              "role" => instance.configuration.role,
+              "runtime" => instance.configuration.runtime,
+              "source_code_hash" => instance.configuration.code_sha_256,
+              "timeout" => instance.configuration.timeout.to_s,
           }
-          resources["aws_db_instance.#{module_name_of(instance)}"] = {
-              "type" => "aws_db_instance",
-              "primary" => {
-                  "id" => instance.db_instance_identifier,
-                  "attributes" => attributes
-              }
+          unless instance.configuration.vpc_config.nil?
+            # lambda is only supported in one vpc, hardcoding
+            attributes["vpc_config.#"] = "1"
+
+            attributes["vpc_config.0.security_group_ids.#"] =
+              instance.configuration.vpc_config.security_group_ids.count.to_s
+            instance.configuration.vpc_config.security_group_ids.each do |sg|
+              crc = Zlib.crc32(sg)
+              attributes["vpc_config.0.security_group_ids.#{crc}"] = sg
+            end
+
+            attributes["vpc_config.0.subnet_ids.#"] =
+              instance.configuration.vpc_config.subnet_ids.count.to_s
+            instance.configuration.vpc_config.subnet_ids.each do |sn|
+              crc = Zlib.crc32(sn)
+              attributes["vpc_config.0.subnet_ids.#{crc}"] = sn
+            end
+          end
+          resources["aws_lambda_function.#{module_name_of(instance)}"] = {
+            "type" => "aws_lambda_function",
+            "primary" => {
+              "id" => instance.configuration.function_name,
+              "attributes" => attributes
+            }
           }
 
           resources
@@ -38,16 +69,22 @@ module Terraforming
 
       private
 
-      def lambda_functions
-        @client.list_functions.functions
-               .inject([]) do |resources, lambda_function|
-          func_detail = @client.get_function(
-            { function_name: lambda_function.function_name }
-          )
-          download_lambda_code(func_detail.code.location,
-                               "#{func_detail.configuration.function_name}.zip")
-          resources.push(func_detail)
+      def download_all_lambda_function_code(all_lambda_functions)
+        all_lambda_functions.each do |function|
+          download_lambda_code(function.code.location,
+                               "#{function.configuration.function_name}.zip")
+        end
+      end
 
+      def lambda_functions_with_code
+        functions = lambda_functions
+        download_all_lambda_function_code(functions)
+        functions
+      end
+
+      def lambda_functions
+        @client.list_functions.functions.inject([]) do |resources, lf|
+          resources.push(@client.get_function(function_name: lf.function_name))
           resources
         end
       end
@@ -74,7 +111,7 @@ module Terraforming
       end
 
       def module_name_of(lambda_function)
-        normalize_module_name(lambda_function.function_name)
+        normalize_module_name(lambda_function.configuration.function_name)
       end
     end
   end
