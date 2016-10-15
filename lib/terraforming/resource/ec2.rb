3a2d92ab36007f4bc8152570e6c0a5f5e27fe5a7
Riley Shott <rileyshott+git@gmail.com>
2016-10-14 18:09:35 -0700

    Add additional arguments to export for ec2
    * Added disable_api_termination argument
    * Added iam_instance_profile argument
    * Fixed bug with source_dest_check argument
    * Simplified in_vpc? method logic

--- lib/terraforming/resource/ec2.rb
+++ lib/terraforming/resource/ec2.rb
@@ -28,9 +28,11 @@ module Terraforming
             "ami" => instance.image_id,
             "associate_public_ip_address" => "true",
             "availability_zone" => instance.placement.availability_zone,
+            "disable_api_termination" => disable_api_termination(instance).to_s,
             "ebs_block_device.#" => ebs_block_devices_in(block_devices, instance).length.to_s,
             "ebs_optimized" => instance.ebs_optimized.to_s,
             "ephemeral_block_device.#" => "0", # Terraform 0.6.1 cannot fetch this field from AWS
+            "iam_instance_profile" => iam_instance_profile?(instance) ? iam_instance_profile(instance) : "",
             "id" => instance.instance_id,
             "instance_type" => instance.instance_type,
             "monitoring" => monitoring_state(instance).to_s,
@@ -40,7 +42,7 @@ module Terraforming
             "public_ip" => instance.public_ip_address,
             "root_block_device.#" => root_block_devices_in(block_devices, instance).length.to_s,
             "security_groups.#" => in_vpc ? "0" : instance.security_groups.length.to_s,
-            "source_dest_check" => instance.source_dest_check.to_s,
+            "source_dest_check" => source_dest_check(instance).to_s,
             "tenancy" => instance.placement.tenancy,
             "vpc_security_group_ids.#" => in_vpc ? instance.security_groups.length.to_s : "0",
           }
@@ -48,7 +50,7 @@ module Terraforming
           placement_group = instance.placement.group_name
           attributes["placement_group"] = placement_group unless placement_group.empty?
 
-          attributes["subnet_id"] = instance.subnet_id if in_vpc?(instance)
+          attributes["subnet_id"] = instance.subnet_id if in_vpc
 
           resources["aws_instance.#{module_name_of(instance)}"] = {
             "type" => "aws_instance",
@@ -80,24 +82,40 @@ module Terraforming
         instance.block_device_mappings.select { |bdm| bdm.ebs.volume_id == volume_id }[0]
       end
 
+      def describe_instance_attribute(instance, attribute)
+        @client.describe_instance_attribute(instance_id: instance.instance_id, attribute: attribute)
+      end
+
+      def disable_api_termination(instance)
+        describe_instance_attribute(instance, 'disableApiTermination')['disable_api_termination']['value']
+      end
+
       def ebs_block_devices_in(block_devices, instance)
         block_devices.reject do |bd|
           root_block_device?(block_device_mapping_of(instance, bd.volume_id), instance)
         end
       end
 
-      #
-      # NOTE(dtan4):
-      #   Original logic is here:
-      #     https://github.com/hashicorp/terraform/blob/281e4d3e67f66daab9cdb1f7c8b6f602d949e5ee/builtin/providers/aws/resource_aws_instance.go#L481-L501
-      #
       def in_vpc?(instance)
-        !vpc_security_groups_of(instance).empty? ||
-          (instance.subnet_id && instance.subnet_id != "" && instance.security_groups.empty?)
+        !instance.vpc_id.nil?
+      end
+
+      def iam_instance_profile(instance)
+        return nil if instance.iam_instance_profile.nil?
+        instance.iam_instance_profile.arn.split('/').last
+      end
+
+      def iam_instance_profile?(instance)
+        iam_instance_profile(instance).nil? ? false : true
       end
 
       def monitoring_state(instance)
         %w(enabled pending).include?(instance.monitoring.state)
+      end
+
+      # source_dest_check can sometimes be an empty string
+      def source_dest_check(instance)
+        [true, false].include?(instance.source_dest_check) ? instance.source_dest_check : true
       end
 
       def instances
@@ -116,10 +134,6 @@ module Terraforming
 
       def root_block_devices_in(block_devices, instance)
         block_devices.select { |bd| root_block_device?(block_device_mapping_of(instance, bd.volume_id), instance) }
-      end
-
-      def vpc_security_groups_of(instance)
-        instance.security_groups.select { |security_group| /\Asg-/ =~ security_group.group_id }
       end
     end
   end
