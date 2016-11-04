25b4f79f760d889537eed1c4c968a5b04264db86
Jim Conner <jimconn@redapt.com>
2016-11-03 20:34:02 -0700

    Added tf_state method back in...need it after all. Who knew? And added rspec tests. Test work on local dev env.

--- lib/terraforming/resource/efs.rb
+++ lib/terraforming/resource/efs.rb
@@ -1,5 +1,6 @@
 # author: Jim Conner <snafu.x@gmail.com>
 # Nov 2016
+require 'awesome_print'
 module Terraforming
   module Resource
     class EFS
@@ -19,6 +20,37 @@ module Terraforming
 
       def tf
         apply_template(@client, "tf/elastic_filesystem")
+      end
+
+      def tfstate
+        idx = -1
+
+        efsystems.inject({}) do  |resources, efs|
+          idx += 1
+
+          attributes = {
+            "creation_token" => efs.creation_token,
+            "id" => efs.file_system_id,
+            "performance_mode" => efs.performance_mode,
+            "tags.%" => "1",
+            "tags.Name" => efs.name,
+          }
+
+          resources["aws_efs_file_system.efs.#{idx}"] = {
+            "type" => "aws_efs_file_system",
+            "depends_on" => [],
+            "primary" => {
+              "id" => efs.file_system_id,
+              "attributes" => attributes,
+              "meta" => {},
+              "tainted" => false,
+            },
+            "deposed" => [],
+            "provider" => "aws",
+          }
+
+          resources
+        end
       end
 
       private
