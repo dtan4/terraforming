b8ac730e01fd6a20a3645cca4c49bd0d23b8e411
Daisuke Fujita <dtanshi45@gmail.com>
2017-07-27 01:57:43 +0900

    Add icmp_code and icmp_type to egress

--- spec/lib/terraforming/resource/network_acl_spec.rb
+++ spec/lib/terraforming/resource/network_acl_spec.rb
@@ -76,6 +76,18 @@ module Terraforming
                 },
               },
               {
+                rule_number: 15000,
+                protocol: "1",
+                rule_action: "allow",
+                egress: true,
+                cidr_block: "0.0.0.0/0",
+                port_range: nil,
+                icmp_type_code: {
+                  code: -1,
+                  type: 4
+                },
+              },
+              {
                 rule_number: 32767,
                 protocol: "-1",
                 rule_action: "deny",
@@ -155,6 +167,17 @@ resource "aws_network_acl" "fuga" {
         icmp_type  = "10"
     }
 
+    egress {
+        from_port  = 0
+        to_port    = 0
+        rule_no    = 15000
+        action     = "allow"
+        protocol   = "1"
+        cidr_block = "0.0.0.0/0"
+        icmp_code  = "-1"
+        icmp_type  = "4"
+    }
+
     tags {
         "Name" = "fuga"
     }
@@ -186,7 +209,7 @@ resource "aws_network_acl" "fuga" {
               "primary" => {
                 "id" => "acl-5678efgh",
                 "attributes" => {
-                  "egress.#" => "0",
+                  "egress.#" => "1",
                   "id" => "acl-5678efgh",
                   "ingress.#" => "2",
                   "subnet_ids.#" => "2",
