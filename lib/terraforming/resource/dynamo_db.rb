a42c26710198e1086017f337f47371aa25b749f5
Shimpei Otsubo <potsbo@gmail.com>
2017-09-19 15:32:40 +0900

    Wip tfstate

--- lib/terraforming/resource/dynamo_db.rb
+++ lib/terraforming/resource/dynamo_db.rb
@@ -7,12 +7,37 @@ module Terraforming
         self.new(client).tf
       end
 
+      def self.tfstate(client: Aws::DynamoDB::Client.new)
+        self.new(client).tfstate
+      end
+
       def initialize(client)
         @client = client
       end
 
       def tf
         apply_template(@client, "tf/dynamo_db")
+      end
+
+      def tfstate
+        tables.inject({}) do |resources, table|
+          attributes = {
+            "arn" => table.table_arn,
+            "attribute.#" => table.attribute_definitions.length.to_s,
+          }
+          resources["aws_dynamodb_table.#{table.table_name}"] = {
+            "type" => "aws_dynamodb_table",
+            "depends_on": [], #TODO(potsbo): check this
+            "primary": {
+              "id": nil, #TODO(potsbo): write here
+              "attributes": attributes,
+              "meta": {},#TODO(potsbo): check this
+              "tainted": false#TODO(potsbo): check this
+            },
+            "deposed": [],#TODO(potsbo): check this
+            "provider": "",#TODO(potsbo): check this
+          }
+        end
       end
 
       def tables
