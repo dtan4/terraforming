5a3bcd15745c004b36c333719b87abbcf9e3a829
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2018-10-06 17:38:03 +0200

    tfstate method basic implementation

--- lib/terraforming/resource/cloud_front_distribution.rb
+++ lib/terraforming/resource/cloud_front_distribution.rb
@@ -19,6 +19,32 @@ module Terraforming
         apply_template(@client, "tf/cloud_front_distribution")
       end
 
+      def tfstate
+        distributions.inject({}) do |resources, distribution|
+          attributes = {
+            "arn" => distribution.arn,
+            "comment" => distribution.comment,
+            "domain_name" => distribution.domain_name,
+            "enabled" => distribution.enabled,
+            "http_version" => distribution.http_version,
+            "id" => distribution.id,
+            "is_ipv6_enabled" => distribution.is_ipv6_enabled,
+            "price_class" => distribution.price_class,
+            "status" => distribution.status,
+          }
+
+          resources["aws_cloudfront_distribution.#{distribution.id}"] = {
+            "type" => "aws_cloudfront_distribution",
+            "primary" => {
+              "id" => distribution.id,
+              "attributes" => attributes
+            }
+          }
+
+          resources
+        end
+      end
+
       private
 
       def distributions
