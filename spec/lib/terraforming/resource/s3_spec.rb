e5556b6e3b15171511f70f546fc7a513e015cf2e
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2019-07-07 17:49:07 +0100

    Added more tests coverage

--- spec/lib/terraforming/resource/s3_spec.rb
+++ spec/lib/terraforming/resource/s3_spec.rb
@@ -43,6 +43,66 @@ module Terraforming
         { location_constraint: "" }
       end
 
+      let(:bucket_tags) do
+        [
+          {
+            key: "marketing",
+            value: "organization"
+          }
+        ]
+      end
+
+      let(:bucket_cors) do
+        [
+          {
+            allowed_headers: [
+              "Authorization",
+            ],
+            allowed_methods: [
+              "GET",
+            ],
+            allowed_origins: [
+              "*",
+            ],
+            max_age_seconds: 3000
+          }
+        ]
+      end
+
+      let(:bucket_lifecycle_configuration) do
+        [
+          {
+            id: "Move rotated logs to Glacier",
+            status: "Enabled",
+            prefix: "rotated/",
+            transitions: [
+              {
+                days: 365,
+                storage_class: "GLACIER"
+              }
+            ]
+          }
+        ]
+      end
+
+      let(:routing_rules) do
+        [
+          {
+            condition: {
+                http_error_code_returned_equals: "404",
+                key_prefix_equals: "index.html"
+            },
+            redirect: {
+                host_name: "www.example.com",
+                http_redirect_code: "",
+                protocol: "http",
+                replace_key_prefix_with: "",
+                replace_key_with: ""
+            }
+          }
+        ]
+      end
+
       context "from ap-northeast-1" do
         let(:client) do
           Aws::S3::Client.new(region: "ap-northeast-1", stub_responses: true)
@@ -55,6 +115,14 @@ module Terraforming
             "NoSuchBucketPolicy",
           ])
           client.stub_responses(:get_bucket_location, [hoge_location, fuga_location, piyo_location])
+          client.stub_responses(:get_bucket_tagging, { tag_set: bucket_tags })
+          client.stub_responses(:get_bucket_cors, { cors_rules: bucket_cors })
+          client.stub_responses(:get_bucket_lifecycle_configuration, { rules: bucket_lifecycle_configuration })
+          client.stub_responses(:get_bucket_website, {
+            error_document: { key: "error.html" },
+            index_document: { suffix: "index.html" },
+            routing_rules: routing_rules
+          })
         end
 
         describe ".tf" do
@@ -82,13 +150,48 @@ resource "aws_s3_bucket" "hoge" {
 }
 POLICY
     tags {
+        marketing = "organization"
+    }
+    cors_rule {
+        allowed_methods = ["GET"]
+        allowed_origins = ["*"]
+        allowed_headers = ["Authorization"]
+        max_age_seconds = 3000
     }
     versioning {
-      enabled = false
+        enabled = false
     }
     logging {
-      target_bucket = "TargetBucket"
-      target_prefix = "TargetPrefix"
+        target_bucket = "TargetBucket"
+        target_prefix = "TargetPrefix"
+    }
+    lifecycle_rule {
+        id      = "Move rotated logs to Glacier"
+        prefix  = "rotated/"
+        enabled =  true
+        transition {
+            storage_class = "GLACIER"
+            days = 365
+        }
+    }
+    website {
+        index_document = "index.html"
+        error_document = "error.html"
+        routing_rules = [
+  {
+    "condition": {
+      "http_error_code_returned_equals": "404",
+      "key_prefix_equals": "index.html"
+    },
+    "redirect": {
+      "host_name": "www.example.com",
+      "http_redirect_code": "",
+      "protocol": "http",
+      "replace_key_prefix_with": "",
+      "replace_key_with": ""
+    }
+  }
+]
     }
 }
 resource "aws_s3_bucket" "fuga" {
@@ -96,13 +199,48 @@ resource "aws_s3_bucket" "fuga" {
     acl    = "private"
     region = ""
     tags {
+        marketing = "organization"
+    }
+    cors_rule {
+        allowed_methods = ["GET"]
+        allowed_origins = ["*"]
+        allowed_headers = ["Authorization"]
+        max_age_seconds = 3000
     }
     versioning {
-      enabled = false
+        enabled = false
     }
     logging {
-      target_bucket = "TargetBucket"
-      target_prefix = "TargetPrefix"
+        target_bucket = "TargetBucket"
+        target_prefix = "TargetPrefix"
+    }
+    lifecycle_rule {
+        id      = "Move rotated logs to Glacier"
+        prefix  = "rotated/"
+        enabled =  true
+        transition {
+            storage_class = "GLACIER"
+            days = 365
+        }
+    }
+    website {
+        index_document = "index.html"
+        error_document = "error.html"
+        routing_rules = [
+  {
+    "condition": {
+      "http_error_code_returned_equals": "404",
+      "key_prefix_equals": "index.html"
+    },
+    "redirect": {
+      "host_name": "www.example.com",
+      "http_redirect_code": "",
+      "protocol": "http",
+      "replace_key_prefix_with": "",
+      "replace_key_with": ""
+    }
+  }
+]
     }
 }
         EOS
@@ -154,6 +292,10 @@ resource "aws_s3_bucket" "fuga" {
             "NoSuchBucketPolicy",
           ])
           client.stub_responses(:get_bucket_location, [hoge_location, fuga_location, piyo_location])
+          client.stub_responses(:get_bucket_tagging, "NoSuchTagSet")
+          client.stub_responses(:get_bucket_cors, "NoSuchCORSConfiguration")
+          client.stub_responses(:get_bucket_lifecycle_configuration, "NoSuchLifecycleConfiguration")
+          client.stub_responses(:get_bucket_website, "NoSuchWebsiteConfiguration")
         end
 
         describe ".tf" do
@@ -163,14 +305,12 @@ resource "aws_s3_bucket" "piyo" {
     bucket = "piyo"
     acl    = "private"
     region = ""
-    tags {
-    }
     versioning {
-      enabled = false
+        enabled = false
     }
     logging {
-      target_bucket = "TargetBucket"
-      target_prefix = "TargetPrefix"
+        target_bucket = "TargetBucket"
+        target_prefix = "TargetPrefix"
     }
 }
         EOS
